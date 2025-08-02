66b48d9b75e83cda4b80f3eb555fc8f76aeb4446
fourleaf <gundamwing.wei@gmail.com>
2025-08-02 15:01:25 +0800

    init

---
+++ bin/modbus_serial_over_tcp.dart
@@ -0,0 +1,5 @@
+import 'package:modbus_serial_over_tcp/modbus_serial_over_tcp.dart' as modbus_serial_over_tcp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${modbus_serial_over_tcp.calculate()}!');
+}
---
+++ test/modbus_serial_over_tcp_test.dart
@@ -0,0 +1,8 @@
+import 'package:modbus_serial_over_tcp/modbus_serial_over_tcp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
